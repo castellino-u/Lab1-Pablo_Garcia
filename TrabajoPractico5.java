e1380b4de68322de2364326c1d272374e99ed9fc
castellino-u <141928201+castellino-u@users.noreply.github.com>
2023-10-12 08:37:00 -0300

    Update TrabajoPractico5.java

--- TrabajoPractico5.java
+++ TrabajoPractico5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TrabajoPractico3 {
+public class TrabajoPractico5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
