2045303f01f76beedd2f1074f438d3d9752a9bea
castellino-u <141928201+castellino-u@users.noreply.github.com>
2023-10-12 09:02:10 -0300

    Update and rename TrabajoPractico5.java to TrabajoPractico3.java

--- TrabajoPractico3.java
+++ TrabajoPractico3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TrabajoPractico5 {
+public class TrabajoPractico3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
